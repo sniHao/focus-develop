aab75229411dc6a81b6d16e0d54c14628de76728
你浩 <1047583808@qq.com>
2025-06-07 13:37:46 +0800

    perf: naive-ui汉化

--- nuxt3-vue3-ts/app.vue
+++ nuxt3-vue3-ts/app.vue
@@ -1,11 +1,12 @@
 <template>
-  <n-config-provider :theme='theme'>
+  <n-config-provider :theme='theme' :locale='zhCN' :date-locale='dateZhCN' >
     <NuxtPage/>
     <n-global-style/>
   </n-config-provider>
 </template>
 
 <script setup lang='ts'>
+import { dateZhCN, zhCN } from 'naive-ui';
 import type {GlobalTheme} from 'naive-ui';
 const theme = ref<GlobalTheme | null>(null);
 provide('theme', theme);
